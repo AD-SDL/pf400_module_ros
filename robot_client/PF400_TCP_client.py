2be94c7231c4c3cdb0ca9368a099bfdbd4834bc4
Dozgulbas <dozgulbas@anl.gov>
2022-04-26 17:14:22 -0500

    Implemented Move Robot Functions

--- robot_client/PF400_TCP_client.py
+++ robot_client/PF400_TCP_client.py
@@ -3,13 +3,21 @@ import time
 import logging
 
 #Log Configuration
-logging.basicConfig(filename = "robot_client_logs.log", level=logging.DEBUG, format = '[%(levelname)s] [%(name)s] %(message)s')
-# Arm_Robot object is the main object that will be used for operations such as remote connection as well as sending movement commands
+logging.basicConfig(filename = "robot_client_logs.log", level=logging.DEBUG, format = '[%(levelname)s] [%(asctime)s] [%(name)s] %(message)s', datefmt = '%Y-%m-%d %H:%M:%S')
+
 class arm_robot(object):
     """
-    Description: Python interface that allows remote commands to be executed using simple string messages over TCP/IP on PF400 cobot. 
-                 Arm_Robot is the main object that will be used for operations such as remote connection as well as sending movement commands.
-                 Programs are sent to the 10x000 port (first robot port: 101000). A program sent to robot will be executed immediately. 
+    Description: 
+                 - Python interface that allows remote commands to be executed using simple string messages over TCP/IP on PF400 cobot. 
+                 - Arm_Robot is the main object that will be used for operations such as remote connection as well as sending movement commands.
+                 - Programs are sent to the 10x000 port (first robot port: 101000). 
+                 - A program sent to robot will be executed immediately unless there is a prior operation running on the robot. 
+                 - If a second motion command is sent while the referenced robot is moving, the second command is blocked and will not reply until the first motion is complete.
+                 - Blended motion tolerance can be adjusted in the motion profile
+
+    Serial Communication Messages from the Robot:
+                 - Responses begin with a "0" if the command was successful, or a negative error code number
+
     """
     def __init__(self, robot_ID:str, host: str, port: int):
         self.logger = logging.getLogger("PF400_Client")
@@ -25,26 +33,26 @@ class arm_robot(object):
         # Predefined locations for plate transferring oparetions
         self.location_dictionary = {"HomeALL": [-493.56371,-82.630104,1157.130034,-83.036521,90,180], 
                                     "HomeArm": [0,0,0,0,0,0], 
-                                    "OT2_1_plate_rack": [0,0,0,0,0,0],
-                                    "OT2_2_plate_rack": [0,0,0,0,0,0], 
-                                    "OT2_3_plate_rack": [0,0,0,0,0,0], 
-                                    "OT2_4_plate_rack": [0,0,0,0,0,0],
-                                    "OT2_1_above_plate": [0,0,0,0,0,0],
-                                    "OT2_2_above_plate": [0,0,0,0,0,0], 
-                                    "OT2_3_above_plate": [0,0,0,0,0,0], 
-                                    "OT2_4_above_plate": [0,0,0,0,0,0],
-                                    "OT2_1_approach_plate": [0,0,0,0,0,0],
-                                    "OT2_2_approach_plate": [0,0,0,0,0,0], 
-                                    "OT2_3_approach_plate": [0,0,0,0,0,0], 
-                                    "OT2_4_approach_plate": [0,0,0,0,0,0],
-                                    "OT2_1_pick_plate": [0,0,0,0,0,0],
-                                    "OT2_2_pick_plate": [0,0,0,0,0,0], 
-                                    "OT2_3_pick_plate": [0,0,0,0,0,0], 
-                                    "OT2_4_pick_plate": [0,0,0,0,0,0],
-                                    "OT2_1_front": [0,0,0,0,0,0],
-                                    "OT2_2_front": [0,0,0,0,0,0], 
-                                    "OT2_3_front": [0,0,0,0,0,0], 
-                                    "OT2_4_front": [0,0,0,0,0,0],
+                                    "OT2_1_plate_rack": [1,0,0,0,0,0],
+                                    "OT2_2_plate_rack": [2,0,0,0,0,0], 
+                                    "OT2_3_plate_rack": [3,0,0,0,0,0], 
+                                    "OT2_4_plate_rack": [4,0,0,0,0,0],
+                                    "OT2_1_above_plate": [1,0,0,0,0,0],
+                                    "OT2_2_above_plate": [2,0,0,0,0,0], 
+                                    "OT2_3_above_plate": [3,0,0,0,0,0], 
+                                    "OT2_4_above_plate": [4,0,0,0,0,0],
+                                    "OT2_1_approach_plate": [1,0,0,0,0,0],
+                                    "OT2_2_approach_plate": [2,0,0,0,0,0], 
+                                    "OT2_3_approach_plate": [3,0,0,0,0,0], 
+                                    "OT2_4_approach_plate": [4,0,0,0,0,0],
+                                    "OT2_1_pick_plate": [1,0,0,0,0,0],
+                                    "OT2_2_pick_plate": [2,0,0,0,0,0], 
+                                    "OT2_3_pick_plate": [3,0,0,0,0,0], 
+                                    "OT2_4_pick_plate": [4,0,0,0,0,0],
+                                    "OT2_1_front": [1,0,0,0,0,0],
+                                    "OT2_2_front": [2,0,0,0,0,0], 
+                                    "OT2_3_front": [3,0,0,0,0,0], 
+                                    "OT2_4_front": [4,0,0,0,0,0],
                                     "Transfer_A": [0,0,0,0,0,0],
                                     "Transfer_B": [0,0,0,0,0,0],
                                     "Transfer_C": [0,0,0,0,0,0],
@@ -68,7 +76,7 @@ class arm_robot(object):
 
         else:
             PF400.connect((self.host,self.port))
-            self.logger.info('Socket Created')
+            # self.logger.info('Socket Created')
             return PF400     
 
         #self.logger.info('Socket Connected to ' + self.host + " Port:", self.port )
@@ -77,7 +85,7 @@ class arm_robot(object):
 
     def disconnect_robot(self, PF400):
         PF400.close()
-        self.logger.info("TCP/IP client is closed")
+        # self.logger.info("TCP/IP client is closed")
 
     def enable_power(self):
 
@@ -144,7 +152,22 @@ class arm_robot(object):
         # Set default motion profile
         self.set_profile()
 
+        self.check_robot_state()
+
         self.logger.info("Robot initialization is successfully completed!")
+    
+    def set_motion_blend_tolerance(self, tolerance: int = 0):
+        """
+        Description: Gets or sets the InRange property of the selected profile, which is a parameter to set tolorance between blended motions.
+        
+        Paramiters
+                tolerance:  from -1 to 100
+                            -1 means do not stop at the end of motion if blending is possible.  
+                            0 means always stop but do not check the end point error.  
+                            > 0 means wait until close to the end point.  
+                            Larger numbers mean less position error is allowed.
+        """
+        pass
 
     def check_robot_heartbeat(self):
 
@@ -231,13 +254,103 @@ class arm_robot(object):
         else:
             raise Exception("Motion profile takes 8 arguments, {} where given".format(len(profile_dict)))
 
+    def wait_before_next_move(self):
+        """
+        If you want to wait for the robot to stop moving, issue a waitForEom command
+        """
+        pass
+    
+    def set_move_command(self, robot_location, grab: bool = False, release: bool = False):
+        # TODO: FIND THE 5th JOINT VALUE FOR WHEN THE GRABBER IS CLOSE AND OPEN
+        robot_command = "MoveJ 1" 
+
+        for count, location in enumerate(self.location_dictionary[robot_location]):
+            if grab == True and count == 4:
+                robot_command += " 222"
+            elif release == True and count == 4:
+                robot_command += " 227"
+            else:    
+                robot_command += " " + str(location) 
+        robot_command += '\n'
+        
+        return robot_command
+
+    def pick_plate_ot2(self, ot2_ID):
+        
+
+        # OT2_4_above_plate
+        # OT2_2_pick_plate
+        # OT2_3_front
+        # OT2_2_plate_rack
+
+        # self.logger.info("Setting defult values to the motion profile")
+
+        # Set movement commands to complete a pick_plate_ot2 operation
+        move_front = self.set_move_command("OT2_" + str(ot2_ID) + "_front")
+        above_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_above_plate")
+        approach_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_pick_plate")
+        pick_up_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_pick_plate", True, False)
+        above_with_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_above_plate", True, False)
+        front_with_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_front", True, False)
 
 
-    def program_robot_target(self, target_location):
+        pick_up_commands = [move_front, above_plate, approach_plate, pick_up_plate, above_with_plate, front_with_plate]
+
+        for count, command in enumerate(pick_up_commands):
+
+            PF400 = self.connect_robot()
+            try:
+                PF400.send(bytes(command.encode('ascii')))
+                out_msg = PF400.recv(4096).decode("utf-8")
+                self.logger.info(out_msg)
+
+                # TODO: CHECK FOR ERROR RETURN FORM ROBOT FIRST 
+                self.logger.info("[pick_plate_ot2 ID:{}] Robot is moved to the {}th location".format(str(ot2_ID), count+1))
+
+            except socket.error as err:
+                self.logger.error('Failed move the robot {}'.format(err))
+            else:
+                self.disconnect_robot(PF400)  
+
+
+    def drop_plate_ot2(self, ot2_ID):
+
+        # Set movement commands to complete a drop_plate_ot2 operation
+        front_with_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_front", True, False)
+        above_with_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_above_plate", True, False)
+        approach_with_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_pick_plate", True, False)
+        drop_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_pick_plate", False, True)
+        above_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_above_plate")
+        front_plate = self.set_move_command("OT2_" + str(ot2_ID) + "_front")
+
+
+        drop_commands = [front_with_plate, above_with_plate, approach_with_plate, drop_plate, above_plate, front_plate]
+
+        for count, command in enumerate(drop_commands):
+
+            PF400 = self.connect_robot()
+            try:
+                PF400.send(bytes(command.encode('ascii')))
+                out_msg = PF400.recv(4096).decode("utf-8")
+                self.logger.info(out_msg)
+
+                # TODO: CHECK FOR ERROR RETURN FORM ROBOT FIRST 
+                self.logger.info("[pick_plate_ot2 ID:{}] Robot is moved to the {}th location".format(str(ot2_ID), count+1))
+
+            except socket.error as err:
+                self.logger.error('Failed move the robot {}'.format(err))
+            else:
+                self.disconnect_robot(PF400)  
+
+
+    def program_robot_target(self, job:str, robot_ID_list: list):
         """
             Programs the robot to execute sequance of movements from its' current location to a given target location
         """
-        pass 
+        if job.lower() == "transfer":
+            self.logger.info("Executing plate transfer between OT2 ID: {} and OT2 ID: {}".format(robot_ID_list[0],robot_ID_list[1]))
+            self.pick_plate_ot2(robot_ID_list[0])
+            self.drop_plate_ot2(robot_ID_list[1])
 
     def move_single(self, target_location):
         """
@@ -264,10 +377,18 @@ class arm_robot(object):
             self.disconnect_robot(PF400)  
 
     def manualy_move_cartesian(self, target_joint):
-
+        """
+        A Cartesian location specifies the coordinates of a position in space using X, Y, and Z coordinates, and an orientation of the robot tool using yaw, pitch, and roll angles.
+        Depending on the robot kinematics, there may be more than one set of joint angles that puts the robot's gripper at the same Cartesian location.
+        """
         pass
 
     def manualy_move_joints(self, target_joint_location):
+        """
+        An angles location is a collection of the joint angles for the robot.  
+        Joint angles are in units of degrees for rotary axes and in millimeters for linear axes.  
+        By moving all joints to the specified values, the robot moves to an unambiguous position and orientation in space.
+        """
 
         pass
 
@@ -310,5 +431,7 @@ class arm_robot(object):
 if __name__ == "__main__":
     robot = arm_robot("1","192.168.1.81",10000)
     robot.initialize_robot()
-    robot.move_single("HomeALL")
-    
+    # robot.move_single("HomeALL")
+    # robot.pick_plate_ot2(1)
+    # robot.drop_plate_ot2(2)
+    robot.program_robot_target("Transfer",[4,3])
