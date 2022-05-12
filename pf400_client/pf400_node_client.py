7573e42980cdeca5061e4cb94adc113235024084
dozgulbas <dozgulbas@anl.gov>
2022-05-12 18:16:48 -0500

    bug fixes

--- pf400_client/pf400_node_client.py
+++ pf400_client/pf400_node_client.py
@@ -22,8 +22,10 @@ def pf400_transfer_command(job:str, robot_1:str = None, robot_2:str = None):
     while True:
         full_command = command_convert(job, robot_1, robot_2)
         print(full_command)
+        #TODO: NEED A DELAY IF MULTIPLE COMMANDS WERE SENT. OTHERWISE ROBOT WILL SKIP PARTS
         sock.send_string(full_command)
         msg = sock.recv_string()
+        print(msg)
         time.sleep(1)
         msg = msg.split('@')
         # msg_output, msg_error, msg_errorcode = msg[0], msg[1], msg[2]
@@ -39,4 +41,7 @@ def main_null():
     print("This function is not meant to have a main function")
 
 if __name__ == '__main__':
-    pf400_transfer_command("transfer", "bob", "alex")
+    pf400_transfer_command("rack","bob")
+    # pf400_transfer_command("transfer","bob","alex")
+    # pf400_transfer_command("complete")
+
