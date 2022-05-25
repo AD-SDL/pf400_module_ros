330b1bfd19eaee7d76c388fcb40c4fe4493c9ab1
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-05-25 16:11:30 -0500

    clean

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import os
-from setuptools import setup, Command, find_packages
-
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-    user_options = []
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
-    def run(self):
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+from setuptools import setup, find_packages
 
 
-package_name = 'arm_driver_pkg'
+install_requires = []
+with open('requirements.txt') as reqs:
+    for line in reqs.readlines():
+        req = line.strip()
+        if not req or req.startswith('#'):
+            continue
+        install_requires.append(req)
+
+
+##this is weird. 
+package_name = 'PF400_driver'
 
 setup(
-    name=package_name,
+    name='PF400_driver',
     version='0.0.1',
     packages=find_packages(exclude= ['resources', 'test*']),
     data_files=[
@@ -23,15 +23,14 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=install_requires,
     zip_safe=True,
     python_requires=">=3.8",
     maintainer='Doga Ozgulbas and Alan Wang',
     maintainer_email='dozgulbas@anl.gov',
     description='Driver for the PF400 robot arm',
-    url='https://github.com/AD-SDL/arm_driver_pkg.git', 
+    url='https://github.com/AD-SDL/PF400_driver.git', 
     license='MIT License',
-    tests_require=['pytest'],
     entry_points={ 
         'console_scripts': [
              'arm_driver = arm_driver_pkg.arm_driver:main_null',
