601930fc5ede285f614c27455bba5ee9d6bb7061
Dozgulbas <dozgulbas@anl.gov>
2022-05-25 13:38:39 -0500

    Fix find_packages() problem

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-from setuptools import setup
+import os
+from setuptools import setup, Command, find_packages
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
 
 package_name = 'arm_driver_pkg'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name, 'pf400_client'],
+    packages=find_packages(exclude= ['resources', 'test*']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,6 +25,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    python_requires=">=3.8",
     maintainer='Doga Ozgulbas and Alan Wang',
     maintainer_email='dozgulbas@anl.gov',
     description='Driver for the PF400 robot arm',
@@ -29,4 +42,14 @@ setup(
 
         ]
     },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
