091c1116f359d88b68ef7442942c781511461b30
Dozgulbas <dozgulbas@anl.gov>
2022-08-19 18:59:43 -0500

    Implemented move single axis function and new move command creater function

--- pf400_description/pf400_description/TCSJointClient.py
+++ pf400_description/pf400_description/TCSJointClient.py
@@ -8,14 +8,18 @@ from operator import add
 from time import sleep
 from unicodedata import name
 
-from sensor_msgs.msg import JointState
+# from sensor_msgs.msg import JointState
 
 class TCSJointClient:
 
 	commandLock = threading.Lock()
-	joint_state = JointState()
+	# joint_state = JointState()
 
 	def __init__(self, host, port, mode = 0):
+		"""
+        Description: 
+        """
+
 		print("Initializing connection...")
 		self.host = host
 		self.port = port
@@ -24,7 +28,7 @@ class TCSJointClient:
 		self.Connect()
 		self.InitConnectionMode()
 		self.axis_count = 6
-		self.joint_state.name = ["J{}".format(x + 1) for x in range(0, self.axis_count)]
+		# self.joint_state.name = ["J{}".format(x + 1) for x in range(0, self.axis_count)] # Comment out for local testing
 		print("Connection ready")
 
 
@@ -34,21 +38,36 @@ class TCSJointClient:
 		self.above = [60.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 	def load_robot_data(self, data_file_path):
+		"""
+        Description: 
+        """
 		pass #TODO
 
 	def load_robot_commands(self, commands_file_path):
+		"""
+        Description: 
+        """
 		pass #TODO
 
 	def Connect(self):
+		"""
+        Description: 
+        """
 		try:
 			self.connection = telnetlib.Telnet(self.host, self.port, 5)
 		except:
 			raise Exception("Could not establish connection")
 
 	def Disconnect(self):
+		"""
+        Description: 
+        """
 		self.connection.close()
 
 	def SendCommand(self, command):
+		"""
+        Description: 
+        """
 		self.commandLock.acquire()
 		try:
 			if not self.connection:
@@ -72,6 +91,9 @@ class TCSJointClient:
 			self.commandLock.release()
 
 	def InitConnectionMode(self):
+		"""
+        Description: 
+        """
 		if not self.connection:
 			self.Connect()
 		if self.mode == 0:
@@ -84,7 +106,7 @@ class TCSJointClient:
 
 	def GetJointData(self):
 		"""
-        Decription: Locates the robot and returns the joint locations for all 6 joints.
+        Description: Locates the robot and returns the joint locations for all 6 joints.
         """
 		states = self.SendCommand("wherej")
 		joints = states.split(' ')
@@ -93,7 +115,7 @@ class TCSJointClient:
 
 	def RefreshJointState(self):
 		"""
-        Decription: 
+        Description: 
         """
 		joint_array = self.GetJointData()
 		multipliers = [
@@ -107,31 +129,82 @@ class TCSJointClient:
 		# self.joint_state.raw_position = joint_array
 		self.joint_state.position = [state * multiplier for state, multiplier in zip(joint_array, multipliers)]
 
-	def move_end_effector_move_neutral(self):
+	def move_end_effector_neutral(self):
 		"""
-        Decription: 
+        Description: 
         """
 		pass #TODO
 	def move_all_joints_neutral(self):
 		"""
-        Decription: 
+        Description: 
         """
 		pass
+	def find_cartesian_coordinates(self):
+		# Obtain current robot cartesian coordinates
+		# Return an int array with x/y/z yaw/pich/roll
+		pass
+
+	def move_in_one_axis(self, profile:int = 2, axis:str= "x", value:int = 0):
+		"""
+		TODO: TRY THIS FUNCTION TO SEE IF END EFFECTOR MOVES ON A SINGLE AXIS PROPORLY
+		"""
+		cartesian_coordinates = self.find_cartesian_coordinates()
+		if axis == "x":
+			cartesian_coordinates[0] += value
+		elif axis == "y":
+			cartesian_coordinates[1] += value
+		elif axis == "z":
+			cartesian_coordinates[2] += value
+
+		move_command = "MoveC "+ " " + str(profile) + " " + "".join(map(str, cartesian_coordinates))
+		self.SendCommand(move_command)
+
+	def create_move_commad(self, target_joint_locations, profile:int = 2, gripper_close: bool = False, gripper_open: bool = False):
+		"""
+		Description: Creates the movement commands with the given robot_location, profile, gripper closed and gripper open info
+		Parameters:
+				- target_location: Which location the PF400 will move.
+				- profile: Motion profile ID.
+				- gripper_close: If set to TRUE, gripper is closed. If set to FALSE, gripper position will remain same as the previous location. 
+				- gripper_open: If set to TRUE, gripper is opened. If set to FALSE, gripper position will remain same as the previous location.
+		"""
+
+		# Checking unpermitted gripper command
+		if gripper_close == True and gripper_open == True:
+			raise Exception("Gripper cannot be open and close at the same time!")
+			
+		# Setting the gripper location to open or close. If there is no gripper position passed in, target_joint_locations will be used.
+		if gripper_close == "True":
+			target_joint_locations[4] = self.gripper_closed
+		elif gripper_open == "True":
+			target_joint_locations[4] = self.gripper_open
+
+		move_command = "movej" + " " + str(profile) + " " + "".join(map(str, target_joint_locations))
+
+		return move_command
+		
+
 
 	def pick(self, jointPos):
 		"""
-        Decription: 
+        Description: 
         """
+		#------
+		slow_profile = 1
+		fast_profile = 2
 		profile = 2
 		jointClosedPos = jointPos
 
 		jointPos[4] = self.gripper_open
 		jointClosedPos[4] = self.gripper_closed   
+		#------
 
 		abovePos = list(map(add, jointPos, self.above))
 		aboveClosedPos = list(map(add, jointClosedPos, self.above))
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
+		#------
+		
+		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
+		self.create_move_commad(self.pf400_neutral)
 		self.SendCommand(cmd)
 
 
@@ -161,7 +234,7 @@ class TCSJointClient:
 
 	def place(self, jointPos):
 		"""
-        Decription: 
+        Description: 
         """
 		profile = 2
 		jointClosedPos = jointPos
@@ -201,9 +274,16 @@ class TCSJointClient:
 
 	def transfer(self, location1, location2):
 		"""
-        Decription: 
+        Description: 
+		
         """
 		self.pick(location1)
 		self.place(location2)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+	target_joint_angles = "1 1 1 1 1 1"
+	# profile = 2
+	# cmd = "movej" + " " + str(profile) + " " + "".join(map(str, target_joint_angles))  # Moves pf400 to neutral position
+	# print(cmd)
+	# robot = TCSJointClient("127.0.0.1", "8080")
+	# robot.pick(target_joint_angles)
