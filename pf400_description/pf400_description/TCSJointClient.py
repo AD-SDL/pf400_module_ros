0119fab8751a0fa34c5c20a04412c4bbbc4da69a
Dozgulbas <dozgulbas@anl.gov>
2022-08-22 18:48:02 -0500

    Forward Kinematics calculator implemented for the end effector

--- pf400_description/pf400_description/TCSJointClient.py
+++ pf400_description/pf400_description/TCSJointClient.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import rclpy
+import os.path
 import telnetlib
 import threading
+import json
+
 import math
 from operator import add
 from time import sleep
@@ -14,7 +17,7 @@ class TCSJointClient:
 	commandLock = threading.Lock()
 	joint_state = JointState()
 
-	def __init__(self, host, port, mode = 0):
+	def __init__(self, host, port, mode = 0, data_file_path = "robot_data.json", commands_file_path = "robot_commands.json", error_codes_path = "error_codes.json"):
 		"""
         Description: 
         """
@@ -24,8 +27,18 @@ class TCSJointClient:
 		self.port = port
 		self.mode = mode
 		self.connection = None
-		self.Connect()
-		self.InitConnectionMode()
+		robot_data, robot1, motion_profile, locations = self.load_robot_data(data_file_path)
+		self.robot_data = robot_data       
+		# Default Motion Profile Paramiters. Using two profiles for faster and slower movements
+		self.motion_profile = motion_profile
+		# Predefined locations for plate transferring oparetions
+		self.location_dictionary = locations
+		self.commands_list = self.load_robot_commands(commands_file_path)
+		self.error_codes = self.load_error_codes(error_codes_path)
+
+		self.connect()
+		self.init_connection_mode()
+		
 		self.axis_count = 6
 		self.joint_state.name = ["J{}".format(x + 1) for x in range(0, self.axis_count)] # Comment out for local testing
 		print("Connection ready")
@@ -38,17 +51,61 @@ class TCSJointClient:
 
 	def load_robot_data(self, data_file_path):
 		"""
-        Description: 
-        """
-		pass #TODO
+		Decription: Loads the robot identification/motion profile/location data as dictionaries 
+		Parameters: 
+				- data_file_path: Path to data file
+		"""
+		# Setting parent file directory 
+		current_directory = os.path.dirname(__file__)
+		parent_directory = os.path.split(current_directory)[0] 
+		file_path = os.path.join(parent_directory + '/utils/'+ data_file_path)
+
+		# load json file
+		with open(file_path) as f:
+			data = json.load(f)
+
+		f.close()
+
+		return data, data["robot_data"][0], data["robot_data"][0]["motion_profile"],data["robot_data"][0]["locations"][0]
+
 
 	def load_robot_commands(self, commands_file_path):
 		"""
-        Description: 
-        """
-		pass #TODO
+		Decription: Loads the available command list as a list
+		Parameters: 
+				- commands_file_path: Path to command list file
+		"""
+		# Setting parent file directory 
+		current_directory = os.path.dirname(__file__)
+		parent_directory = os.path.split(current_directory)[0] 
+		file_path = os.path.join(parent_directory + '/utils/'+ commands_file_path)
 
-	def Connect(self):
+		# load json file
+		with open(file_path) as f:
+			data = json.load(f)
+
+		f.close()
+		return data["Commands_List"]
+
+	def load_error_codes(self, error_codes_path):
+		"""
+		Decription: Loads the robot error codes data as a dictionary
+		Parameters: 
+				- error_codes_path: Path to error codes data file
+		"""
+		# Setting parent file directory 
+		current_directory = os.path.dirname(__file__)
+		parent_directory = os.path.split(current_directory)[0] 
+		file_path = os.path.join(parent_directory + '/utils/'+ error_codes_path)
+
+		# load json file
+		with open(file_path) as f:
+			data = json.load(f)
+
+		f.close()
+		return data["Error_Codes"]
+
+	def connect(self):
 		"""
         Description: 
         """
@@ -57,13 +114,13 @@ class TCSJointClient:
 		except:
 			raise Exception("Could not establish connection")
 
-	def Disconnect(self):
+	def disconnect(self):
 		"""
         Description: 
         """
 		self.connection.close()
 
-	def SendCommand(self, command):
+	def send_command(self, command):
 		"""
         Description: 
         """
@@ -89,7 +146,7 @@ class TCSJointClient:
 		finally:
 			self.commandLock.release()
 
-	def InitConnectionMode(self):
+	def init_connection_mode(self):
 		"""
         Description: 
         """
@@ -97,26 +154,29 @@ class TCSJointClient:
 			self.Connect()
 		if self.mode == 0:
 			# Set TCS to nonverbose
-			self.SendCommand("mode 0")
+			self.send_command("mode 0")
 		else:
 			# Set TCS to verbose
-			self.SendCommand("mode 1")
-		self.SendCommand("selectrobot 1")
+			self.send_command("mode 1")
+		self.send_command("selectrobot 1")
+		
+	def initilize_robot(self):
+		pass
 
-	def GetJointData(self):
+	def get_joint_data(self):
 		"""
         Description: Locates the robot and returns the joint locations for all 6 joints.
         """
-		states = self.SendCommand("wherej")
+		states = self.send_command("wherej")
 		joints = states.split(' ')
-		joints = joints[1:]
+		joints = joints[1:] # WHY THE FIRST ELEMENT SKIPPED?
 		return [float(x) for x in joints]
 
-	def RefreshJointState(self):
+	def refresh_joint_state(self):
 		"""
         Description: 
         """
-		joint_array = self.GetJointData()
+		joint_array = self.get_joint_data()
 		multipliers = [
 			0.001,			# J1, Z
 			math.pi / 180,	# J2, shoulder
@@ -130,35 +190,71 @@ class TCSJointClient:
 
 	def move_end_effector_neutral(self):
 		"""
-        Description: 
+        Description: Set 
         """
-		pass #TODO
+
+		current_joint_locations = self.get_joint_data()
+		current_joint_locations[3] = 530.993
+		self.send_command(self.create_move_command(current_joint_locations))
 
 	def move_all_joints_neutral(self):
 		"""
         Description: 
         """
-		pass
+
+		self.move_end_effector_neutral()
+		self.send_command(self.create_move_command(self.pf400_neutral))
 
 	def find_cartesian_coordinates(self):
-		# Obtain current robot cartesian coordinates
-		# Return an int array with x/y/z yaw/pich/roll
-		pass
- 
-	def move_in_one_axis(self, profile:int = 2, axis:str= "x", value:int = 0):
+		"""
+        Description: This function finds the current cartesian coordinates and angles of the robot.
+		Return: A float array with x/y/z yaw/pich/roll
+        """
+
+		coordinates = self.send_command("whereC")
+		coordinates_list = coordinates.split(' ')
+		coordinates_list = coordinates_list[1:] # WHY THE FIRST ELEMENT SKIPPED?
+
+		return [float(x) for x in coordinates_list]
+
+	def forward_kinematics(self, joint_states):
+
+		cartesian_coordinates = self.find_cartesian_coordinates()
+		shoulder_lenght = 225
+		elbow_lenght = 210
+
+		# Convert angles to radians
+		shoulder_angle = joint_states[1]*math.pi/180 #Joint 2 
+		elbow_angle = joint_states[2]*math.pi/180 #Joint 3
+
+		x = shoulder_lenght*math.cos(shoulder_angle) + elbow_lenght*math.cos(shoulder_angle+elbow_angle)
+		y = shoulder_lenght*math.sin(shoulder_angle) + elbow_lenght*math.sin(shoulder_angle+elbow_angle)
+		z = joint_states[0]
+
+		cartesian_coordinates[0] = x
+		cartesian_coordinates[1] = y
+		cartesian_coordinates[2] = z
+		print(x, y, z)
+
+		return cartesian_coordinates
+
+	def move_in_one_axis(self, target_location, profile:int = 2, axis_x:int= 0,axis_y:int= 0, axis_z:int= 0):
 		"""
 		TODO: TRY THIS FUNCTION TO SEE IF END EFFECTOR MOVES ON A SINGLE AXIS PROPERLY
 		"""
-		cartesian_coordinates = self.find_cartesian_coordinates()
-		if axis == "x":
-			cartesian_coordinates[0] += value
-		elif axis == "y":
-			cartesian_coordinates[1] += value
-		elif axis == "z":
-			cartesian_coordinates[2] += value
+		# First move robot on linear rail
+		current_joint_state = self.get_joint_data()
+		current_joint_state[5] = target_location[5]
+		self.send_command(self.create_move_command(current_joint_state)
+		)
+		cartesian_coordinates = self.forward_kinematics(target_location)
+
+		cartesian_coordinates[0] += axis_x
+		cartesian_coordinates[1] += axis_y
+		cartesian_coordinates[2] += axis_z
 
 		move_command = "MoveC "+ " " + str(profile) + " " + "".join(map(str, cartesian_coordinates))
-		self.SendCommand(move_command)
+		self.send_command(move_command)
 
 	def create_move_command(self, target_joint_locations, profile:int = 2, gripper_close: bool = False, gripper_open: bool = False):
 		"""
@@ -168,6 +264,7 @@ class TCSJointClient:
 				- profile: Motion profile ID.
 				- gripper_close: If set to TRUE, gripper is closed. If set to FALSE, gripper position will remain same as the previous location. 
 				- gripper_open: If set to TRUE, gripper is opened. If set to FALSE, gripper position will remain same as the previous location.
+		Return: Returns the created movement command in string format
 		"""
 
 		# Checking unpermitted gripper command
@@ -184,100 +281,67 @@ class TCSJointClient:
 
 		return move_command
 		
-
-
-	def pick_plate(self, jointPos):
+	def pick_plate(self, target_pose):
 		"""
         Description: 
         """
 		#------
 		slow_profile = 1
 		fast_profile = 2
-		profile = 2
-		jointClosedPos = jointPos
+		jointClosedPos = target_pose
 
-		# jointPos[4] = self.gripper_open
+		# target_pose[4] = self.gripper_open
 		# jointClosedPos[4] = self.gripper_closed   
+
+
 		#------
 
-		abovePos = list(map(add, jointPos, self.above))
+		abovePos = list(map(add, target_pose, self.above))
 		aboveClosedPos = list(map(add, jointClosedPos, self.above))
 		#------
-		
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
-		
-		cmd =self.create_move_command(self.pf400_neutral)
-		self.SendCommand(cmd)
 
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, abovePos))  # Moves pf400 to neutral position
-		
-		cmd = self.create_move_command(abovePos, slow_profile, False, True)
-		self.SendCommand(cmd)
+		# move_neutral
+		# move_front 
+        # above_plate 
+        # approach_plate 
+        # pick_up_plate 
+        # above_with_plate
+        # front_with_plate 
 
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, jointPos))  # Moves pf400 to neutral position
-        # cmd = "movej" + " " + str(profile) + " " + str(self.jointPos[0]) + " " + str(self.jointPos[1]) + " " + str(self.jointPos[2]) + " " + str(self.jointPos[3])+ " " + str(self.gripper_open) + " " + str(self.jointPos[5])        
-		
-		cmd = self.create_move_command(jointPos, slow_profile, False, True)
-		self.SendCommand(cmd)
+		self.move_all_joints_neutral()
+		self.send_command(self.create_move_command(abovePos, fast_profile, False, True))
+		self.send_command(self.create_move_command(target_pose, slow_profile, False, True))
 		sleep(0.5)
-
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, jointClosedPos))  # Moves pf400 to neutral position
-		
-		cmd = self.create_move_command(jointPos, slow_profile, True, False)
-		self.SendCommand(cmd)
+		self.send_command(self.create_move_command(target_pose, slow_profile, True, False))
 		sleep(0.5)
+		self.send_command(self.create_move_command(abovePos, slow_profile, True, False))
+		self.move_all_joints_neutral()
+		# TODO: USE BELOW MOVE_ONE_AXIS FUNCTIONS TO MOVE ABOVE AND FRONT OF THE EACH TARGET LOCATIONS
+		# self.move_in_one_axis(target_pose, profile = 2, axis_x = 60, axis_y = 0, axis_z = 60)
+		# self.move_in_one_axis(target_pose, profile = 1, axis_x = 0, axis_y = 0, axis_z = 60)
 
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, aboveClosedPos))  # Moves pf400 to neutral position
-		cmd = self.create_move_command(abovePos, slow_profile, True, False)
-
-		self.SendCommand(cmd)
-
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
-		# self.SendCommand(cmd)
-
-
-	def place_plate(self, jointPos):
+	def place_plate(self, target_pose):
 		"""
         Description: 
         """
+		slow_profile = 1
+		fast_profile = 2
 
-		profile = 2
-		jointClosedPos = jointPos
+		jointClosedPos = target_pose
 
-		jointPos[4] = self.gripper_open
+		target_pose[4] = self.gripper_open
 		jointClosedPos[4] = self.gripper_closed   
 
-		abovePos = list(map(add, jointPos, self.above))
+		abovePos = list(map(add, target_pose, self.above))
 		aboveClosedPos = list(map(add, jointClosedPos, self.above))
 
-		# cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
-		cmd =self.create_move_command(self.pf400_neutral)
+		self.move_all_joints_neutral()
+		self.send_command(self.create_move_command(abovePos, slow_profile, True, False))
+		self.send_command(self.create_move_command(target_pose, slow_profile, True, False))
+		self.send_command(self.create_move_command(target_pose, slow_profile, False, True))
+		self.send_command(self.create_move_command(abovePos))
 
-		self.SendCommand(cmd)
 
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, aboveClosedPos))  # Moves pf400 to neutral position
-		self.SendCommand(cmd)
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, jointClosedPos))  # Moves pf400 to neutral position
-        # cmd = "movej" + " " + str(profile) + " " + str(self.jointPos[0]) + " " + str(self.jointPos[1]) + " " + str(self.jointPos[2]) + " " + str(self.jointPos[3])+ " " + str(self.gripper_open) + " " + str(self.jointPos[5])        
-
-		self.SendCommand(cmd)
-
-		sleep(0.5)
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, jointPos))  # Moves pf400 to neutral position
-
-		self.SendCommand(cmd)
-
-		sleep(0.5)
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, abovePos))  # Moves pf400 to neutral position
-
-		self.SendCommand(cmd)
-
-		cmd = "movej" + " " + str(profile) + " " + " ".join(map(str, self.pf400_neutral))  # Moves pf400 to neutral position
-		self.SendCommand(cmd)
 
 	def transfer(self, location1, location2):
 		"""
