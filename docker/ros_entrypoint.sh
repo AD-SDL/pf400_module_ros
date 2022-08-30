1ded0ff8899c09c2cd1259b2df92b9859253166b
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-08-29 22:39:09 -0500

    small docker fix

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -5,13 +5,4 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 source "$ROS_WS/install/setup.bash" --
 
-# create robot_config.yaml based on robot_ip environment variable
-if [[ -z $robot_ip ]]; then
-    echo "[ERROR] robot_ip environment variable is not set. Must be set to construct the needed robot config"
-    exit 1
-else
-    # echo $robot_ip
-    python3 ~/create_rc.py -ip $robot_ip
-fi
-
 exec "$@"
