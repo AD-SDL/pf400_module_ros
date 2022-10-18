381dce24f47b531b591e7bb20a9f65bc551e5fa3
Dozgulbas <dozgulbas@anl.gov>
2022-10-18 11:55:20 -0500

    Working on the object colors

--- pf400_description/launch/pf400_module_rviz.launch.py
+++ pf400_description/launch/pf400_module_rviz.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
 
  
   pkg_share = FindPackageShare(package='pf400_description').find('pf400_description')
-  default_rviz_config_path = os.path.join(pkg_share, 'config/pf400_module_rviz_config.rviz')
+  default_rviz_config_path = os.path.join(pkg_share, 'config/workcell_config.rviz')
  
   default_urdf_model_path = os.path.join(pkg_share, 'urdf/PF400_module.urdf') 
  
