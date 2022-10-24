44e35b517ee2dde6024c4fa444a3dc1875ea9f38
Dozgulbas <dozgulbas@anl.gov>
2022-10-24 12:49:39 -0500

    Adding a new package to obtain results from completed plates with camera

--- plate_camera_client/plate_camera_client/plate_camera_client.py
+++ plate_camera_client/plate_camera_client/plate_camera_client.py
@@ -14,12 +14,12 @@ from wei_services.srv import WeiActions
 from pf400_driver.pf400_driver import PF400
 from pf400_driver.pf400_camera_driver import CAMERA
 
-class PF400CameraNode(Node):
+class PlateCameraNode(Node):
     '''
     The jointControlNode inputs data from the 'action' topic, providing a set of commands for the driver to execute. It then receives feedback, 
     based on the executed command and publishes the state of the peeler and a description of the peeler to the respective topics.
     '''
-    def __init__(self, NODE_NAME = "PF400_Camera_Node"):
+    def __init__(self, NODE_NAME = "Plate_Camera_Node"):
         '''
         The init function is neccesary for the peelerNode class to initialize all variables, parameters, and other functions.
         Inside the function the parameters exist, and calls to other functions and services are made so they can be executed in main.
@@ -38,9 +38,6 @@ class PF400CameraNode(Node):
         self.stateTimer = self.create_timer(timer_period, self.stateCallback)
         self.statePub = self.create_publisher(String, NODE_NAME + '/state', 10)
         self.stateTimer = self.create_timer(timer_period, self.stateCallback)
-
-        # self.whereSrv = self.create_service(Empty, NODE_NAME + "/pf400_whereJ", self.whereJCallback)
-        # self.moveSrv = self.create_service(MoveJ, NODE_NAME + "/pf400_moveJ", self.moveJCallback)
 
         self.action_handler = self.create_service(WeiActions, NODE_NAME + "/action_handler", self.actionCallback)
         self.description_handler = self.create_service(WeiDescription, NODE_NAME + "/description_handler", self.descriptionCallback)
