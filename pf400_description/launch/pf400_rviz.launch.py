ab4813718bdf1cfcd805a28104060415658a0a3f
Dozgulbas <dozgulbas@anl.gov>
2023-01-09 14:50:57 -0600

    Renaming the xacro files to module xacros

--- pf400_description/launch/pf400_rviz.launch.py
+++ pf400_description/launch/pf400_rviz.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
 
   pkg_share = FindPackageShare(package='pf400_description').find('pf400_description')
   default_rviz_config_path = os.path.join(pkg_share, 'config/pf400_rviz_config.rviz')
-  default_urdf_model_path = os.path.join(pkg_share, 'urdf/pf400_robot.xacro') 
+  default_urdf_model_path = os.path.join(pkg_share, 'urdf/pf400_module.xacro') 
  
   fake_hardware = LaunchConfiguration('fake_hardware')
   urdf_model = LaunchConfiguration('urdf_model')
